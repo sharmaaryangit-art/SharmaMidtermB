61973f273566c2799de0c70905309682fa09b5e7
sharmaaryangit-art <sharmaaryangit@gmail.com>
2026-06-16 20:43:19 +0300

    Add project files.

---
+++ SharmaMidtermB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SharmaMidtermB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
