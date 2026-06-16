d8bf81c8795fcea7a8067db36e20f3e13f48b5c8
sharmaaryangit-art <sharmaaryangit@gmail.com>
2026-06-16 20:57:39 +0300

    question #4: Player Model

---
+++ SharmaMidtermB/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace SharmaMidtermB.Models {
+    public class Player {
+        public int PlayerId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Position { get; set; } = null!; 
+        public int ShirtNumber { get; set; }
+        public DateTime FirstMatch { get; set; }
+    }
+}
