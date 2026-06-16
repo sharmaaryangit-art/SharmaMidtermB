670f063ec065c6d977ebcc8825353d1c50c30ff8
sharmaaryangit-art <sharmaaryangit@gmail.com>
2026-06-16 21:03:10 +0300

    question #6: Adding Player Controller

---
+++ SharmaMidtermB/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using SharmaMidtermB.Data;
+
+namespace SharmaMidtermB.Controllers {
+    public class PlayerController : Controller {
+        private readonly ApplicationDbContext _context;
+
+        public PlayerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Players.ToListAsync());
+        }
+    }
+}
